dc3b75ad224860433304a571c8e04b9e1acd17da
Qiki <qi@brandingbrand.com>
2015-07-01 00:30:16 -0400

    Initial Commit

---
+++ @_@/@_@/@_@/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  @_@
+//
+//  Created by kiki on 7/1/15.
+//  Copyright (c) 2015 kiki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ @_@/@_@/@_@/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  @_@
+//
+//  Created by kiki on 7/1/15.
+//  Copyright (c) 2015 kiki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
